00806027c32ab1ebb98deb9cebf8ce8d649255bb
Wallysson <wally.nm@gmail.com>
2024-03-28 19:40:05 -0300

    Updates github config

--- svelte.config.js
+++ svelte.config.js
@@ -9,19 +9,16 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: `${base}/404.html`
+			pages:  'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		}),
-		alias: {
-			// this will match a file
-			lib: '/src/lib',
-		},
 		paths: {
-            base
-        }
+			base
+		}
 	}
 };
 
